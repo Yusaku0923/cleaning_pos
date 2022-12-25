a76f158b7eaeeec76870cfaf46cc418246768252
yusaku.nakayama <nakayama8039@gmail.com>
2022-12-25 13:19:49 +0900

    アイコン追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- icon -->
+    <link rel="apple-touch-icon" href="{{ public_path('img/icon.png') }}" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
